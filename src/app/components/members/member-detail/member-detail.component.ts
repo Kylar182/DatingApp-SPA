74fbcfc68fb3515962ad7cc526a3651d2afe19d9
Unknown <ajbeeman@kfsoftwaresolutions.com>
2020-02-08 03:23:22 -0600

    End of Section 9

--- src/app/components/members/member-detail/member-detail.component.ts
+++ src/app/components/members/member-detail/member-detail.component.ts
@@ -6,6 +6,7 @@ import { error } from 'util';
 import { UserService } from 'src/app/_services/user.service';
 import { Country } from 'src/app/_models/Country.enum';
 import { Gender } from 'src/app/_models/Gender.enum';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -16,22 +17,41 @@ export class MemberDetailComponent implements OnInit {
   user: User;
   countryType: typeof Country = Country;
   genderType: typeof Gender = Gender;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private userService: UserService, private alertify: AlertifyService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.user = data['user'];
+      this.user = data.user;
     });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
   }
 
-  // loadUser() {
-  //   this.userService.getUser(this.route.snapshot.params.id).subscribe((user: User) => {
-  //     this.user = user;
-  //   }, error => {
-  //     this.alertify.error(error);
-  //   });
-  // }
+  getImages() {
+    const imageUrls = [];
+    for (const photo of this.user.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+        description: photo.description
+      });
+    }
 
+    return imageUrls;
+  }
 }
